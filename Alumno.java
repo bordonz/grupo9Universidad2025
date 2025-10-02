fabfacbef11b691b06d8c319360df8714b79336a
Ayelen Guadalupe Aguero <agueroayelen10@gmail.com>
2025-10-02 19:27:50 -0300

    Vuelvo a adjuntar clase alumno

--- Alumno.java
+++ Alumno.java
@@ -1,88 +1,81 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entidades;
 
 import java.time.LocalDate;
 
-/**
- *
- * @author pc
- */
 public class Alumno {
-    private int idAlumno = -1;
-    private int dni;
-    private String apellido;
-    private String nombre;
-    private LocalDate fechaDeNacimiento;
-    private boolean estado;
+        private int idAlumno = -1;
+        private int dni;
+        private String apellido;
+        private String nombre;
+        private LocalDate fechaDeNacimiento;
+        private boolean estado;
 
-    public Alumno() {
-    }
-    
-    public Alumno(int dni, String apellido, String nombre, LocalDate fechaDeNacimiento, boolean estado) {
-        idAlumno = -1;
-        this.dni = dni;
-        this.apellido = apellido;
-        this.nombre = nombre;
-        this.fechaDeNacimiento = fechaDeNacimiento;
-        this.estado = estado;
-    }
+         public Alumno() {
+         }
+        
 
-    public int getIdAlumno() {
-        return idAlumno;
-    }
+        public Alumno(int dni, String apellido, String nombre, LocalDate fechaDeNacimiento, boolean estado) {
+            this.dni = dni;
+            this.apellido = apellido;
+            this.nombre = nombre;
+            this.fechaDeNacimiento = fechaDeNacimiento;
+            this.estado = estado;
+        }
 
-    public void setIdAlumno(int idAlumno) {
-        this.idAlumno = idAlumno;
-    }
+        public int getIdAlumno() {
+            return idAlumno;
+        }
 
-    public int getDni() {
-        return dni;
-    }
+        public void setIdAlumno(int idAlumno) {
+            this.idAlumno = idAlumno;
+        }
 
-    public void setDni(int dni) {
-        this.dni = dni;
-    }
+        public int getDni() {
+            return dni;
+        }
 
-    public String getApellido() {
-        return apellido;
-    }
+        public void setDni(int dni) {
+            this.dni = dni;
+        }
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
+        public String getApellido() {
+            return apellido;
+        }
 
-    public String getNombre() {
-        return nombre;
-    }
+        public void setApellido(String apellido) {
+            this.apellido = apellido;
+        }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+        public String getNombre() {
+            return nombre;
+        }
 
-    public LocalDate getFechaDeNacimiento() {
-        return fechaDeNacimiento;
-    }
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
 
-    public void setFechaDeNacimiento(LocalDate fechaDeNacimiento) {
-        this.fechaDeNacimiento = fechaDeNacimiento;
-    }
+        public LocalDate getFechaDeNacimiento() {
+            return fechaDeNacimiento;
+        }
 
-    public boolean isEstado() {
-        return estado;
-    }
+        public void setFechaDeNacimiento(LocalDate fechaDeNacimiento) {
+            this.fechaDeNacimiento = fechaDeNacimiento;
+        }
 
-    public void setEstado(boolean estado) {
-        this.estado = estado;
-    }
+        public boolean isEstado() {
+            return estado;
+        }
 
-    @Override
-    public String toString() {
-        return "Alumno{" + "idAlumno=" + idAlumno + ", dni=" + dni + ", apellido=" + apellido + ", nombre=" + nombre + ", fechaDeNacimiento=" + fechaDeNacimiento + ", estado=" + estado + '}';
-    }
-    
-    
-}
+        public void setEstado(boolean estado) {
+            this.estado = estado;
+        }
+
+        @Override
+        public String toString() {
+            return "Alumno{" + "idAlumno=" + idAlumno + ", dni=" + dni + ", apellido=" + apellido + ", nombre=" + nombre + ", fechaDeNacimiento=" + fechaDeNacimiento + ", estado=" + estado + '}';
+        }
+
+        
+        }
+
+
