b6906e74727c8e3ed3c1cba7aac0b1444409857d
MCMathot <mcmathot@yahoo.com.ar>
2025-10-02 20:22:09 -0300

    Metodo alumnoNuevo
    En clase AlumnoData

--- src/persistencia/AlumnoData.java
+++ src/persistencia/AlumnoData.java
@@ -2,9 +2,12 @@ package persistencia;
 
 import entidades.Alumno;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import javax.naming.spi.DirStateFactory;
 
 public class AlumnoData {
 
@@ -14,7 +17,34 @@ public class AlumnoData {
         this.conec = conexion.buscarConexion();
     }
 
-    public Alumno buscaAlumno(int num) {
+    public boolean alumnoNuevo(Alumno a) {
+        String query = "INSERT INTO alumno (dni, apellido, nombre, fecha, estado) VALUES(?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = conec.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, a.getDni());
+            ps.setString(2, a.getApellido());
+            ps.setString(3, a.getNombre());
+            ps.setDate(4, Date.valueOf(a.getFechaDeNacimiento()));
+            ps.setBoolean(5, a.isEstado());
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                a.setIdAlumno(rs.getInt(1));
+            } else {
+                System.out.println("No se pudo obtener el ID");
+            }
+            ps.close();
+            System.out.println("Guardado");
+            return true;
+        } catch (SQLException e) {
+            System.out.println("No se pudo insertar al alumno");
+            return false;
+        }
+
+    }
+
+    public Alumno buscarAlumno(int num) {
         Alumno a = null;
         String query = "Select * FROM alumno WHERE idAlumno = ?";
         try {
