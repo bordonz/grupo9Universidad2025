22af60655db55efceffa1ca46754cd5544357a1d
cristianb1983 <crisbenegas83@gmail.com>
2025-10-02 18:34:16 -0300

    clase MiConexion

--- src/persistencia/MiConexion.java
+++ src/persistencia/MiConexion.java
@@ -1,5 +1,32 @@
 package persistencia;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MiConexion {
-    
+
+    private String url;
+    private String usuario;
+    private String password;
+
+    private static Connection conexion = null;
+
+    public MiConexion(String url, String usr, String pass) {
+
+    }
+
+    public Connection buscarConexion() {
+        if (conexion == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                conexion = DriverManager.getConnection(url, usuario, password);
+            } catch (SQLException | ClassNotFoundException ex) {
+                System.out.println("No se puede conectar o no se puede cargar el driver.Error: " + ex.getMessage());
+            }
+        }
+        return conexion;
+    }
 }
+
+
